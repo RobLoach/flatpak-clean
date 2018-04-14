9c6cf1e900309784df249d6023cae0e3f67c5a5d
Rob Loach <robloach@gmail.com>
2018-04-14 15:46:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# flatpak-clean
+
+Uninstalls all [Flatpak](https://flatpak.org/) applications, and removes all configuration files.
+
+WARNING: USE AT OWN RISK
+
+## Install
+
+```
+wget https://raw.githubusercontent.com/RobLoach/flatpak-clean/master/flatpak-clean.sh
+chmod +x flatpak-clean.sh
+```
+
+## Usage
+
+```
+./flatpak-clean.sh
+```
+
+## License
+
+MIT
---
+++ flatpak-clean.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Flatpak Clean
+# http://github.com/robloach/flatpak-clean
+#
+# This will uninstall all user and system Flatpak applications.
+#
+# Warning: USE AT OWN RISK.
+#
+# License: MIT
+
+# Uninstall all Flatpak user-space applications.
+APPS=$(flatpak list --user)
+if [ -n "${APPS}" ]; then
+	for i in $APPS ; do
+		flatpak uninstall --user --force-remove $i || true
+	done
+fi
+
+# Uninstall all Flatpak system-space applications.
+APPS=$(flatpak list --system)
+if [ -n "${APPS}" ]; then
+	for i in $APPS; do
+		flatpak uninstall --system --force-remove $i || true
+	done
+fi
+
+# Remove the Flatpak app configurations.
+rm -rf ~/.var/app
